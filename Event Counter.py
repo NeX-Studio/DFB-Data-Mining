07dd1a639539ea33aea6c550d27b354c6ec9bf21
Weili An <China_Aisa@live.com>
2017-02-13 00:03:16 +0800

    Finish data classifier

--- Event Counter.py
+++ Event Counter.py
@@ -2,6 +2,7 @@ import csv
 events=set()
 discipline=set()
 sport=set()
+country=set()
 with open('Summer Olympic medallists 1896 to 2008.csv',newline='') as rawdata:
     with open('eventType.csv', 'w', newline='') as db:
         datareader=csv.DictReader(rawdata)
@@ -11,7 +12,8 @@ with open('Summer Olympic medallists 1896 to 2008.csv',newline='') as rawdata:
             events.add(row['Event'])
             discipline.add(row['Discipline']);
             sport.add(row['Sport'])
+            country.add(row['NOC'])
             counter=counter+1
             if counter%5000==0:
                 print("[+]"+str(counter)+" row")
-        database.writerows([events,discipline,sport])
+        database.writerows([events,discipline,sport,country])
