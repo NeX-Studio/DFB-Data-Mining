b6562dc0d05bbdbf3578ad015f7593d42a09b126
Weili An <China_Aisa@live.com>
2017-02-13 08:45:47 +0800

    BUG Delected

--- data classification.py
+++ data classification.py
@@ -9,7 +9,7 @@ with open("eventType.csv") as events:
     #eventlist=eventlist.split(',')
     medal=['Gold','Silver','Bronze'] # In order to get index
     for event in discipline:
-        #with open()
+        # Bug: WRONG DATA
         with open('csv_file/%s_data.csv'%''.join(event.split(" ")),'w') as db:
             with open("Summer Olympic medallists 1896 to 2008.csv",newline='') as rawdata:
                 data=csv.DictReader(rawdata) # Initialize every loop
@@ -17,15 +17,18 @@ with open("eventType.csv") as events:
                 database.writeheader()
                 medaldict=dict()
                 medaldict.update([(country,[0,0,0]) for country in countrylist])
+                temp=medaldict.copy()
                 for i in range(1896,2012,4):
                     for row_d in data:
                         if eval(row_d['Edition'])<i: # Loop until we get the year(Naive solution, eval for tranform str into int)
                             continue
                         elif eval(row_d['Edition'])>i: # Avoid over looping
                             break
-                        medaldict[row_d['NOC']][medal.index(row_d['Medal'])]+=1 # Gain 1 for medal!
-                    medaldict.update({'Edition':i})
-                    database.writerow(medaldict)
+                        temp[row_d['NOC']][medal.index(row_d['Medal'])]+=1 # Gain 1 for medal!
+                    temp.update({'Edition':i})
+                    database.writerow(temp)
+                    temp.clear()
+                    temp.update([(country,[0,0,0]) for country in countrylist])
 
 
 
