26d4e75d2280acc580f09d4cd91fd482137ac879
Weili An <China_Aisa@live.com>
2017-02-14 17:22:58 +0800

    Solving bug

--- data classification.py
+++ data classification.py
@@ -9,27 +9,29 @@ with open("eventType.csv") as events:
     #eventlist=eventlist.split(',')
     medal=['Gold','Silver','Bronze'] # In order to get index
     for event in discipline:
-        # Bug: WRONG DATA
         with open('csv_file/%s_data.csv'%''.join(event.split(" ")),'w') as db:
-            with open("Summer Olympic medallists 1896 to 2008.csv",newline='') as rawdata:
-                data=csv.DictReader(rawdata) # Initialize every loop
-                database=csv.DictWriter(db,fieldnames=countryinit) # Use country as header
-                database.writeheader()
-                medaldict=dict()
-                medaldict.update([(country,[0,0,0]) for country in countrylist])
-                temp=medaldict.copy()
-                for i in range(1896,2012,4):
+            database=csv.DictWriter(db,fieldnames=countryinit) # Use country as header
+            database.writeheader()
+            medaldict=dict()
+            medaldict.update([(country,[0,0,0]) for country in countrylist])
+            temp=medaldict.copy()
+            for i in range(1896,2012,4):
+                with open("Summer Olympic medallists 1896 to 2008.csv",newline='') as rawdata:
+                    data=csv.DictReader(rawdata) # Initialize every loop
                     for row_d in data:
-                        if row_d['Discipline'] != event: # Check if it is the right competition
-                            continue
-                        if eval(row_d['Edition'])<i: # Loop until we get the year(Naive solution, eval for tranform str into int)
-                            continue
-                        elif eval(row_d['Edition'])>i: # Avoid over looping
+                        #print(row_d['Discipline']+"\t"+event) # Dict is not at order!
+                        #print("matched")
+                        if eval(row_d['Edition'])>i:
                             break
+                        if eval(row_d['Edition'])<i:
+                            continue
+                        if row_d['Discipline'] != event: # Check if it is the right competition
+                            #print("Dismatch")
+                            continue
                         temp[row_d['NOC']][medal.index(row_d['Medal'])]+=1 # Gain 1 for medal!
                     temp.update({'Edition':i})
                     database.writerow(temp)
-                    print(temp)
+                    #print(temp)
                     temp.clear()
                     temp.update([(country,[0,0,0]) for country in countrylist])
 
